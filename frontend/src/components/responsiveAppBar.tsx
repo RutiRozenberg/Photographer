acb31efbfd215c838bfdd6e492b66355a1f3381c
RutiRozenberg <ruti7633@gmail.com>
2024-09-01 20:30:07 +0300

    Added content to the services component.

--- frontend/src/components/responsiveAppBar.tsx
+++ frontend/src/components/responsiveAppBar.tsx
@@ -1,18 +1,21 @@
-import { MenuItem,Tooltip, Button, Avatar, Container, Menu, Box , AppBar ,Toolbar, IconButton,Typography } from '@mui/material';
+import { MenuItem, Tooltip, Button, Avatar, Container, Menu, Box, AppBar, Toolbar, IconButton, Typography, useTheme } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import CameraEnhanceRoundedIcon from '@mui/icons-material/CameraEnhanceRounded';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useNavigate  } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-const pages = [{text:'Services', url:'/services'},{text:'Meetings', url:'/meetings'}];
-const settings = [{text:'profile', url:'/'},{text: 'Logout', url:'/'}];
+const pages = [{ text: 'Services', url: '/services' }, { text: 'Meetings', url: '/meetings' }];
+const settings = [{ text: 'profile', url: '/' }, { text: 'Logout', url: '/' }];
 
 export default function ResponsiveAppBar() {
+
+  const theme = useTheme();
+
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
-  
-  const navigate = useNavigate ();
+
+  const navigate = useNavigate();
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -21,137 +24,164 @@ export default function ResponsiveAppBar() {
     setAnchorElUser(event.currentTarget);
   };
 
-  const handleCloseNavMenu = (url:string) => {
+  const handleCloseNavMenu = (url: string) => {
     setAnchorElNav(null);
     navigate(url);
   };
 
-  const handleCloseUserMenu = (url:string) => {
+  const handleCloseUserMenu = (url: string) => {
     setAnchorElUser(null);
     navigate(url);
   };
 
   return (
-    <AppBar position="static" component="div">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <CameraEnhanceRoundedIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component={Link}
-            to="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'default',
-              fontWeight: 700,
-              letterSpacing: '.2rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Racheli
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="Open Navigation Menu"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
+    <>
+      <AppBar
+        position="static"
+        component="div"
+        sx={{
+          color: theme.palette.primary.dark,
+          backgroundColor: 'white',
+        }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <CameraEnhanceRoundedIcon
+              sx=
+              {{
+                display: { xs: 'none', md: 'flex' },
+                mr: 1,
+                fontSize: 30,
+              }} />
+            <Typography
+              variant="h6"
+              noWrap
+              component={Link}
+              to="/"
               sx={{
-                display: { xs: 'block', md: 'none' },
+                fontSize: 30,
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'default',
+                fontWeight: 700,
+                letterSpacing: '.1rem',
+                textDecoration: 'none',
+                color: theme.palette.primary.dark
               }}
             >
-              {pages.map((page) => (
-                <MenuItem key={page.text} onClick={()=>handleCloseNavMenu(page.url)} >
-                  <Typography textAlign="center">{page.text}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <CameraEnhanceRoundedIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component={Link}
-            to="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'default',
-              fontWeight: 700,
-              letterSpacing: '.2rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            Racheli
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page, index) => (
-              <Button
-                key={index}
-                onClick={()=>handleCloseNavMenu(page.url)}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page.text}
-              </Button>
-            ))}
-          </Box>
+              Racheli
+            </Typography>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="Open Navigation Menu"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
               </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page.text} onClick={() => handleCloseNavMenu(page.url)} >
+                    <Typography textAlign="center">{page.text}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+            <CameraEnhanceRoundedIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+            <Typography
+              variant="h5"
+              noWrap
+              component={Link}
+              to="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'default',
+                fontWeight: 700,
+                letterSpacing: '.1rem',
+                color: 'inherit',
+                textDecoration: 'none',
               }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
             >
-              {settings.map((setting, index) => (
-                <MenuItem key={index} onClick={()=>handleCloseUserMenu(setting.url)}>
-                  <Typography textAlign="center">{setting.text}</Typography>
-                </MenuItem>
+              Racheli
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page, index) => (
+                <Button
+                  key={index}
+                  onClick={() => handleCloseNavMenu(page.url)}
+                  sx=
+                  {{
+                    my: 2,
+                    display: 'block',
+                    fontWeight: 500,
+                    color: theme.palette.primary.dark
+                  }}
+                >
+                  {page.text}
+                </Button>
               ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar
+                    sx={{
+                      p: 0,
+                      backgroundColor: theme.palette.secondary.main,
+                      color: theme.palette.secondary.light,
+                    }} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                {settings.map((setting, index) => (
+                  <MenuItem key={index} onClick={() => handleCloseUserMenu(setting.url)}>
+                    <Typography textAlign="center">{setting.text}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Outlet />
+    </>
   );
 }
--- frontend/src/components/services.component.tsx
+++ frontend/src/components/services.component.tsx
@@ -1,10 +1,48 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../store/store"
+import { fetchServices } from "../store/features/services.slice";
+import { Box, Typography, useTheme } from "@mui/material";
 
 export default function Services() {
 
-    return (
-      <>
-      </>
-    )
-  }
-  
-   
+  const services = useAppSelector(state => state.service.services);
+  const dispatch = useAppDispatch();
+  const theme = useTheme();
+
+  useEffect(() => {
+    dispatch(fetchServices());
+  }, [dispatch]);
+
+  return (
+    <>
+      <Box
+        sx={{
+          width: '100%',
+          background: `linear-gradient(90deg, ${theme.palette.primary.main} 40%, ${theme.palette.primary.dark} 80%)`,
+          paddingTop: '10vh',
+          paddingBottom : '10vh',
+          display: 'flex',
+          justifyContent: 'center',
+          boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+        }}
+      >
+        <Typography
+          variant="h1"
+          align="center"
+          sx={{
+            color: theme.palette.secondary.dark,
+            fontSize: '3rem',
+            letterSpacing: '0.1em',
+            fontWeight: 700,
+          }}
+        >
+          Our Services
+        </Typography>
+      </Box>
+      {services.map((service) => (
+        <div key={service.id}>{service.name}</div>
+      ))}
+    </>
+  )
+}
+
